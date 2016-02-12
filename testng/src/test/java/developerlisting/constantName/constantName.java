8ff6c95e9d1a2887342ff7c06f82aad11ef49874
Wongsakorn <wongsakorn@ddproperty.com>
2016-02-12 15:43:25 +0700

    Add constant name

--- testng/src/test/java/developerlisting/constantName/constantName.java
+++ testng/src/test/java/developerlisting/constantName/constantName.java
@@ -19,11 +19,22 @@ public class constantName {
     /********************** Page **********************/
     public static String PageNhLanding = "03-Nh Landing Page";
     public static String PageNhSearchResult = "03-Nh Search Result Page";
+    public static String PageNhListingDetailPage = "03-Nh Listing Detail Page";
 
     /********************** Widget Test **********************/
-    public static String WidgetNenuBar = "04-Menu bar";
+    public static String WidgetMenuBar = "04-Menu bar";
     public static String WidgetFeaturedNewhomesCarousel = "04-Featured Newhomes Carousel";
     public static String WidgetFeaturedExploreUpcoming = "04-Featured Explore Upcoming";
+    public static String WidgetSearchOption = "04-Search Option";
+    public static String WidgetMobileContactBottomStickyBar = "04-Contact Bottom Sticky Bar";
+    public static String WidgetTableContentAndUnitType = "04-Table contain and Unit Type";
 
-
+    /********************** Widget Test **********************/
+    /*
+    All urls will be moved into page object class
+     */
+    public static String NhLandingPageSG = "http://www.propertyguru.com.sg/new-project-launch";
+    public static String NhListingSearchResultPageMY = "http://www.%spropertyguru.com.my/new-property-launch/1";
+    public static String NhListingSearchResultPageSG = "http://www.%spropertyguru.com.sg/new-project-launch/1";
+    public static String NhListingPropertyReviewSearchResultPageSG = "http://www.%spropertyguru.com.sg/new-project-launch/reviews?disableCache=true";
 }
